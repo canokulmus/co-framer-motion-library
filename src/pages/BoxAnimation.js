7dd808c90298aff6b92363f205befc0f6b2ff09d
CAN OKULMUS <e2400265@ceng.metu.edu.tr>
2022-11-23 04:00:57 +0300

    update: box animation

--- src/pages/BoxAnimation.js
+++ src/pages/BoxAnimation.js
@@ -3,12 +3,31 @@ import { Input } from '../components/Input';
 import { motion } from 'framer-motion';
 import "../css/boxAnimation.css"
 
+const initialValues = {
+    x: 0,
+    y: 0,
+    opacity: 100,
+    borderWidth: 4,
+    scale: 100,
+    rotate: 0,
+}
 
 const BoxAnimation = () => {
-    const [x, setX] = useState(0);
-    const [y, setY] = useState(0);
-    const [scale, setScale] = useState(100);
-    const [rotate, setRotate] = useState(0);
+    const [x, setX] = useState(initialValues.x);
+    const [y, setY] = useState(initialValues.y);
+    const [opacity, setOpacity] = useState(initialValues.opacity);
+    const [borderWidth, setBorderWidth] = useState(initialValues.borderWidth);
+    const [scale, setScale] = useState(initialValues.scale);
+    const [rotate, setRotate] = useState(initialValues.rotate);
+
+    const reset = () => {
+        setX(initialValues.x);
+        setY(initialValues.y);
+        setOpacity(initialValues.opacity);
+        setBorderWidth(initialValues.borderWidth);
+        setScale(initialValues.scale);
+        setRotate(initialValues.rotate);
+    }
 
     return (
         <div className=' h-screen flex justify-center items-center'>
@@ -16,8 +35,9 @@ const BoxAnimation = () => {
                 <div className=''>
                     <motion.div
                         className="box"
-                        animate={{ x, y, rotate, scale: scale / 100 }}
+                        animate={{ x, y, rotate, scale: scale / 100, opacity: opacity / 100 }}
                         transition={{ type: "spring" }}
+                        style={{ borderWidth }}
                     />
                 </div>
 
@@ -35,7 +55,17 @@ const BoxAnimation = () => {
                     <Input value={scale} set={setScale} min={50} max={150}>
                         scale
                     </Input>
+                    <Input value={opacity} set={setOpacity} min={20} max={100}>
+                        opacity
+                    </Input>
+                    <Input value={borderWidth} set={setBorderWidth} min={1} max={10}>
+                        border
+                    </Input>
+
                 </div>
+                <button className="bg-pink hover:opacity-90 text-white font-bold py-2 px-4 rounded my-2" onClick={reset}>
+                    Reset
+                </button>
             </div>
         </div>
     )
